5cc93302f1c5eb89c8df0452734f7912c8c4aa15
habib <habib.benbrahim98@gmail.com>
2025-04-08 00:13:25 +0100

    Text Glow, navbar, dynamic card

--- Glowup/src/app/app.routes.ts
+++ Glowup/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+// import { ShopComponent } from './shop/shop.component';
+// import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: 'home', component: HomeComponent },
+  // { path: 'shop', component: ShopComponent },
+  // { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' } 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
